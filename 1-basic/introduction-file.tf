09319168d3c8f7f356b348061839014c67129e37
Mayank Sharma <smayank252@gmail.com>
2022-01-31 18:27:12 +0530

    Add files via upload

--- 1-basic/introduction-file.tf
+++ 1-basic/introduction-file.tf
@@ -10,7 +10,7 @@ terraform {
 
 # Provider Block
 provider "aws" {
-  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  profile = "mayank" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
 
@@ -18,4 +18,5 @@ provider "aws" {
 resource "aws_instance" "ec2demo" {
   ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
+  availability_zone = "us-east-1b"
 }
